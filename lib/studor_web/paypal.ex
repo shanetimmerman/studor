d7b56de0711bebce467b6d239afbb78986e10609
shanetimmerman <shanetimmerman@gmail.com>
2018-11-25 10:14:05 -0500

    sales working

--- lib/studor_web/paypal.ex
+++ lib/studor_web/paypal.ex
@@ -6,18 +6,19 @@ defmodule Paypal do
         HTTPoison.start
 
         url = <<"https://api.sandbox.paypal.com/v1/oauth2/token">>
+        api_user = "Ad9n9s7seoBirJwOod2lM91dPD077QedgscyggyWPqY7JxY01_vTtF67ZlrjiQwlZ7QNxKdYo5HAEias"
+        api_password = "EIMiv4tz9pkVBitXdLgzN5KRtgfaaNhfzG7blKLE3sV-d2cPcwD5O5QsrYMJtQzDgTaDkChhQqeEXNTC"
+        api_credentials = api_user <> ":" <> api_password
         headers = [
             {"Accept", "application/json"},
             {"Accept-Language", "en_US"},
-            {"Authorization", "Basic base64 encoded string"},
+            # {"Authorization", "Basic #{Base.encode64(api_credentials)}"},
         ]
-        api_user = api_user
-        api_password = api_password
         # TODO user not authenicated properly (something about -u option of curl
         # and how Poison handles it)
         options = [
             hackney: [basic_auth: {api_user, api_password}],
-            # grant_type: "client_credentials"
+            grant_type: "client_credentials"
         ]
 
         {:ok, resp} = HTTPoison.get(url, headers, options)
@@ -25,35 +26,53 @@ defmodule Paypal do
         Jason.decode!(resp.body)
     end
 
-    def pay(api_signature, payer_email, reciever_email) do
+    def pay(api_token, payer_email, reciever_email) do
         HTTPoison.start
-
         url = "https://api.sandbox.paypal.com/v1/payments/payment"
         headers = [
             {"Content-Type", "application/json"},
-            {"Authorization", "Bearer Access-Token"}
+            {"Authorization", "Bearer #{api_token}"}
         ]
-        body = [
-            {"intent", "sale"},
-            {"payer", [
-                {"payment_method", "paypal"},
-                {"payer_info", payer_email},
-            ]},
-            {"transactions", [
-                {"amount", [
-                    {"total", "30.11"},
-                    {"currency", "USD"},
-                ]},
-                {"payee", {"email", reciever_email}},
-                {"description", "A tutoring session."},
-                {"payment_options", {"allowed_payment_method", "INSTANT_FUNDING_SOURCE"},},
-                {"soft_descriptor", "ECHI5786786"},
-            ]},
-            {"redirect_urls", [
-                {"return_url", "http://localhost:4000"},
-                {"cancel_url", "http://localhost:4000"},
-            ]},
-        ]
+        body = Jason.encode!(%{
+            intent: "sale",
+            payer: %{
+                payment_method: "paypal",
+                payer_info: %{
+                    email: payer_email,
+                }
+            },
+
+            application_context: %{
+                brand_name: "Stutor",
+                shipping_preference: "NO_SHIPPING",
+                user_action: "commit",
+            },
+
+            transactions: [
+                %{
+                    description: "A tutoring session.",
+                    soft_descriptor: "ECHI5786786",
+
+                    amount: %{
+                        total: "30.11",
+                        currency: "USD",
+                    },
+
+                    payee: %{
+                        email: reciever_email,
+                    },
+
+                    payment_options: %{
+                        allowed_payment_method: "INSTANT_FUNDING_SOURCE"
+                    },
+                },
+            ],
+
+            redirect_urls: %{
+                return_url: "http://localhost:4000",
+                cancel_url: "http://localhost:4000",
+            },
+        })
 
         {tag, resp} = HTTPoison.post(url, body, headers, [])
         # TODO RETURN SALE ID or ERROR, handle in session controller
